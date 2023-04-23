7a7ecadc7d5643180cb0fea8448e0684846d3e24
Zafeer Hafeez <contact@zafeer.pk>
2023-04-23 06:46:00 +0500

    test: add test case and test data

--- __test__.cpp
+++ __test__.cpp
@@ -1,26 +1,43 @@
-#include "./lib/CSVReader.hpp"
 #include <iostream>
-#include <string>
+#include <cassert>
+#include "./lib/CSVReader.hpp"
+
 using namespace std;
 
 int main()
 {
-    CSVReader csvReader("./data/test.csv");
+    // Test constructor and read function
+    CSVReader reader("./data/test.csv");
+    bool success = reader.read();
+    assert(success);
 
-    csvReader.read();
+    // Test getData function
+    std::vector<std::vector<std::string>> data = reader.getData();
+    assert(data.size() == 5);
+    assert(data[0].size() == 3);
+    assert(data[1][2] == "2.0");
+    assert(data[2][1] == "cat");
 
-    csvReader.printData();
+    // Test strip function
+    assert(reader.strip("  apple ") == "apple");
 
-    cout << csvReader.findColByName("Values") << endl;
+    // Test findColByName function
+    int index = reader.findColByName("C");
+    assert(index == 2);
 
-    csvReader.printValuesByCol("hello");
+    // Test getValuesByCol function
+    std::vector<std::string> values = reader.getValuesByCol("B");
+    assert(values.size() == 4);
+    assert(values[0] == "dog");
+    assert(values[2] == "banana");
 
-    auto values = csvReader.getValuesByCol("Values");
+    // Testing the words with spaces
+    assert(data[4][1] == "hello world");
 
-    for (auto value = values.begin(); value != values.end(); value++)
-    {
-        cout << stoi(*value) << endl;
-    }
+    // Test printValuesByCol function
+    reader.printValuesByCol("C");
+
+    std::cout << "All test cases passed!" << std::endl;
 
     return 0;
 }
